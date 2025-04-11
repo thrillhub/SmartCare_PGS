4dfbbbca06c9c58dc5fcc0c8825794a467056c46
thrillhub <aakashyadav94486@gmail.com>
2025-04-11 18:18:17 +0545

    updated 11

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  presets: [
-    ["@babel/preset-env", { modules: false }], // Tree-shaking friendly
-    "@babel/preset-react"
-  ],
-  ignore: [], // Ensures all files are processed
-  minified: false, // Disable minification (helps debugging)
-};
+    presets: [
+      "@babel/preset-env", 
+      "@babel/preset-react"  
+    ]
+  };
