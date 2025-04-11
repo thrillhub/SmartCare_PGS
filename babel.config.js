4c7a749fccc69a9ecf2dcc49487934b49c303b49
thrillhub <aakashyadav94486@gmail.com>
2025-04-11 18:16:46 +0545

    updated 11 april

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    presets: [
-      "@babel/preset-env", 
-      "@babel/preset-react"  
-    ]
-  };
+  presets: [
+    ["@babel/preset-env", { modules: false }], // Tree-shaking friendly
+    "@babel/preset-react"
+  ],
+  ignore: [], // Ensures all files are processed
+  minified: false, // Disable minification (helps debugging)
+};
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,42 @@
-// src/app/layout.js (Server component)
-import { Inter } from "next/font/google";
+"use client"; 
+
+import React from "react"; 
 import "./globals.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import ClientLayout from "./ClientLayout"; 
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Smart Care Connects",
-  description: "Your Website Description",
-};
+import FloatingButton from "./Components/FloatingButton";
+import { usePathname } from 'next/navigation'; 
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname(); 
+
+
+  const noHeaderFooterPaths = [
+    '/patient-login',
+    '/patient-register',
+    '/doctor-login',
+    '/doctor-register',
+    '/doctor-appointment',
+    '/chat-box',
+    '/doctor-chat-box',
+    '/Patient-profile',
+  ];
+
+  const showHeaderFooter = !noHeaderFooterPaths.includes(pathname);
+
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Smart Care Connects</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className={inter.className}>
-        <ClientLayout>
-          {children}
-        </ClientLayout>
+      <body className="font-inter">
+        {showHeaderFooter && <Header />}
+        {children}
+        {showHeaderFooter && <FloatingButton />}
+        {showHeaderFooter && <Footer />}
       </body>
     </html>
   );
